39339b015ce49c0d3ff70477f299dc1a6ef2c060
Raian Desktop <raiannicacio@id.uff.br>
2023-11-03 10:25:45 -0300

    Limpeza de testes

--- Teste_WebSrapperProxy/UnitTest_Extrator.cs
+++ Teste_WebSrapperProxy/UnitTest_Extrator.cs
@@ -7,13 +7,5 @@ namespace Teste_WebSrapperProxy.Testes
     public class Teste_Extrator
     {
 
-        [Fact]
-        public void ResultadoNaoEVazio()
-        {
-            Extrator extrator = new Extrator();
-
-            extrator.ExtratorHtml();
-           
-        }
     }
 }
